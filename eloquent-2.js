93c0b15a8f0cc478357b1792fdae84412090dbd1
sam-by-name <sam-sanders@hotmail.co.uk>
2018-03-25 19:35:31 +1300

    add comments to eloquent workings

--- eloquent-2.js
+++ eloquent-2.js
@@ -2,26 +2,26 @@
 
 //Looping a triangle
 
-let result = "";
-for (let i = 0; i < 7; i++) {
-  console.log(result = result + "#")
+let result = "";                                   // declare empty variable, to hold results of the for loop.
+for (let i = 0; i < 7; i++) {                      // for loop to iterate 8 times
+  console.log(result = result + "#")               // prints loop results and adds a # each time
 }
 
 //FizzBuzz
 
-for (let i = 1; i <= 100; i++) {
-    let straight = "";
-    let flush = "";
-    let fullHouse = "" ;
-    if (i %3 == 0)  {
-      straight ="Fizz";
-    } if (i %5 == 0) {
-      flush ="Buzz";
-    } if (i %3 == 0 && i %5 == 0) {
-      fullHouse = straight + flush;
+for (let i = 1; i <= 100; i++) {                   // loop to iterate 100 times
+    let straight = "";                             // declare empty variable to be assigned "Fizz" for all divisible by 3
+    let flush = "";                                // declare empty variable to be assigned "Buzz" for all divisible by 5
+    let fullHouse = "" ;                           // declare empty variable to be assigned straight+flush for all divisible by 3+5
+    if (i %3 == 0)  {                              // if the loops "i" variable is divisible by 3
+      straight ="Fizz";                            // This variable gets the value Fizz
+    } if (i %5 == 0) {                             // if the loops "i" variable is divisible by 3
+      flush ="Buzz";                               // This variable gets the value Buzz
+    } if (i %3 == 0 && i %5 == 0) {                // if the loops "i" variable is divisible by 3+5
+      fullHouse = straight + flush;                // This variable gets the value of both straight+flush
     }
       
-    console.log(fullHouse ||flush ||straight||i);
+    console.log(fullHouse ||flush ||straight||i);  // prints 1-100  with "Fizz"for all / 3, "Buzz" for all / 5 and "FizzBuzz" for all / by both
   }
     
     
@@ -29,17 +29,17 @@ for (let i = 1; i <= 100; i++) {
 
 // Chess Board
 
-let size = 10;
-let chess = "";
-for (let i = 0; i < size; i++)  {
-  for (let x = 0; x < size; x++)  {
-    if ((i + x) % 2 == 0) {
-      chess += " ";
-    } else {
-      chess += "#";
+let size = 8;                        // declares variable "size" with value of "10"
+let chess = "";                      // declares variable "chess" but with no value
+for (let i = 0; i < size; i++)  {    // loops by value of variable "size"                                             (second hand on clock)
+  for (let x = 0; x < size; x++)  {  // loops by value of variable "size" and iterates once every full outer loop on  (minute hand on clock)
+    if ((i + x) % 2 == 0) {          // if loop "i" is even
+      chess += " ";                  // variable "chess" gets a space
+    } else {                         // if loop "i" is not even 
+      chess += "#";                  // variable "chess" gets a #
     }
   }
-  chess += "\n";
+  chess += "\n";                     // if loop "x" reaches value of "size", variable "chess" gets a new line
 }
-console.log(chess);
+console.log(chess);                  // prints value of chess (as an equally wide/tall chequers board)
             
